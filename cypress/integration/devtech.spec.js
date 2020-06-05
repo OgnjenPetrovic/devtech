606f842d903962fb65303b5329448e55d7a14aa0
OgnjenPetrovic <zoomght@gmail.com>
2020-06-05 14:56:44 +0200

    Solutions links

--- cypress/integration/devtech.spec.js
+++ cypress/integration/devtech.spec.js
@@ -5,4 +5,26 @@ describe('Integration test with visual testing', function() {
       // Take a snapshot for visual diffing
       cy.percySnapshot();
     });
+
+    //SOLUTIONS LINKS
+    it('TC - 01 Loads the solutions page', function() {
+        cy.visit('solutions/');
+        cy.percySnapshot();
+    });
+    it('TC - 02 Loads the solutions page - application development', function() {
+        cy.visit('solutions/application-development-modernization/');
+        cy.percySnapshot();
+    });
+    it('TC - 03 Loads the solutions page - system platform migration', function() {
+        cy.visit('solutions/system-platform-migration/');
+        cy.percySnapshot();
+    });
+    it('TC - 04 Loads the solutions page - chanel and marcetplace', function() {
+        cy.visit('solutions/channel-marketplace-enablement/');
+        cy.percySnapshot();
+    });
+    it('TC - 05 Loads the solutions page - prdictivce analytics', function() {
+        cy.visit('solutions/predictive-analytics/');
+        cy.percySnapshot();
+    });
   });
