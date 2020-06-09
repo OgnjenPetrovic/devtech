ca3efa278eed55dc3ee428da3fe5b7bec6cf46ee
OgnjenPetrovic <zoomght@gmail.com>
2020-06-09 09:34:19 +0200

    Added all pages

--- cypress/integration/devtech.spec.js
+++ cypress/integration/devtech.spec.js
@@ -53,15 +53,53 @@ describe('Integration test with visual testing', function() {
 
     //CAREERS
 
-    it('TC - 10 Loads the careers page', function() {
+    it('TC - 10 Loads the Careers - search and apply', function() {
         cy.visit('search-and-apply/');
         cy.percySnapshot();
     });
 
-    it('TC - 10 Loads the careers page', function() {
-        cy.visit('search-and-apply/');
+    it('TC - 11 Loads the Careers - training and devlepoment', function() {
+        cy.visit('search-and-apply/training-and-development/');
         cy.percySnapshot();
     });
+
+    it('TC - 12 Loads the Careers - work environment', function() {
+        cy.visit('search-and-apply/work-environment/');
+        cy.percySnapshot();
+    });
+
+    it('TC - 13 Loads the Careers - devtech lab incubator', function() {
+        cy.visit('search-and-apply/devtech-lab-incubator/');
+        cy.percySnapshot();
+    });
+
+    //ABOUT DEVTECH
    
-    
+    it('TC - 14 Loads the About devtech', function() {
+        cy.visit('about-devtech/');
+        cy.percySnapshot();
+    });
+
+    it('TC - 15 Loads the About devtech - leadership', function() {
+        cy.visit('about-devtech/leadership/');
+        cy.percySnapshot();
+    });
+
+    it('TC - 16 Loads the About devtech - newsroom', function() {
+        cy.visit('newsroom/');
+        cy.percySnapshot();
+    });
+
+    //FOOTER LINKS
+
+    it('TC - 17 Privacy policy', function() {
+        cy.visit('privacy-policy/');
+        cy.percySnapshot();
+    });
+
+    it('TC - 18 Contact us', function() {
+        cy.visit('contact-us/');
+        cy.percySnapshot();
+    });
+        
   });
